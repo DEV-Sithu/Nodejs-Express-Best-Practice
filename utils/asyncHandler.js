87a95ac2ac57b94aaae69565da5615db14767db0
mgkaunggithub@gmail.com <mgkaunggithub@gmail.com>
2025-01-31 04:13:26 +0630

    ff

--- utils/asyncHandler.js
+++ utils/asyncHandler.js
@@ -1,8 +1,9 @@
-const asyncHandler = fn => (req, res, next) => 
-    Promise.resolve(fn(req, res, next)).catch(next);
+const asyncHandler = fn => (req, res, next) =>   Promise.resolve(fn(req, res, next)).catch(next);
   
-  // Usage in routes
-  router.get('/', asyncHandler(async (req, res) => {
-    const data = await someAsyncOperation();
-    res.json(data);
-  }));
+  // Usage in controller:
+  // getUser = asyncHandler(async (req, res) => {
+  // const user = await UserService.getUser(req.params.id);
+  // res.json(user);
+  // });
+
+  //module.exports = getUser;
